bb34981254e102160212b34e20927784f6511cd8
Shamik1836 <shamikdas2001@gmail.com>
2021-10-25 13:18:44 +0530

    ADD: Viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,7 +1,57 @@
 import React from "react";
-
+import styled from "styled-components";
 function Viewers() {
-  return <div>Viewers</div>;
+  return (
+    <Container>
+      {" "}
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="" />
+      </Wrap>
+    </Container>
+  );
 }
 
 export default Viewers;
+
+const Container = styled.div`
+  margin-top: 30px;
+  display: grid;
+  cursor: pointer;
+  padding: 30px 0 26px;
+  //   This property below will create five columns grid with width ranging from 0 to the whole width that's why we have used minmax
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+  grid-gap: 10px;
+`;
+
+const Wrap = styled.div`
+  border-radius: 10px;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  transform: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  img {
+    width: 100%;
+    height: 100%;
+    //  This attribute will try to keep the images within the div as much as possible
+    object-fit: cover;
+  }
+  //Wraps the hover to the parent i.e wrap
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
